35a0639981145e2923a460aa9aad0f2c24e51543
Camille Onoda <info@camilleonoda.com>
2023-08-07 17:21:45 +0900

    Loop through an entire list

---
+++ magicians.py
@@ -0,0 +1,7 @@
+# Loop through an entire list
+magicians = ['alice', 'david', 'camille', 'yuzuki', 'jin']
+for magician in magicians:
+    #print(magician)
+    print(f"{magician.title()}, that was a great trick!")
+print("\nCan't wait to see you all again!")
+
