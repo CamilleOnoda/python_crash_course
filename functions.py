ff129dc3f7200f0f3fa9f14d4c28e288320445e7
Camille Onoda <info@camilleonoda.com>
2023-08-27 07:05:17 +0900

    Chapter8 - Functions

---
+++ functions.py
@@ -0,0 +1,36 @@
+# Define a function
+def greet_user():
+    # This is a docstring which describes what the function does. 
+    # When Python generates documentation for the functions, 
+    # it looks for a string immediately after the function definition.
+    """Display a greeting message"""
+    print("Hello!")
+
+# Call the function to be able to use it
+greet_user()
+
+
+# Pass information to a function
+def greet_user(username):
+    print(f"Hello {username.title()}!")
+
+greet_user('camille')
+
+
+# Arguments and parameters
+# The variable 'username' above is a parameter: 
+# a piece of info the function needs to do its job.
+# The value 'camille' is an argument: a piece of info passed from a function call to a function.
+# The argument 'camille' was passed to the function greet_user() and the value was assigned to the parameter 'username'.
+
+
+# Positional arguments. Python must match each argument in the function call with a parameter in the function definition.
+# Based on the order of the arguments provided
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# Multiple function calls
+describe_pet('T-rex', 'Fred')
+describe_pet('rabbit', 'Marie')
---
+++ functions_exercise.py
@@ -0,0 +1,12 @@
+# Message
+def display_message():
+    print("I'm learning about functions! This is Chapter 8 of Python Crash Course.")
+
+display_message()
+
+
+# Favorite book
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
+
+favorite_book("jojos Bizarre Adventures")
