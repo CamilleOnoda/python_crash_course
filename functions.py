6a13613c1770fdca9a614fca06b51e927fd20e3c
Camille Onoda <info@camilleonoda.com>
2023-09-03 08:50:08 +0900

    Keyword arguments: name-value pair in a function call

--- functions.py
+++ functions.py
@@ -20,11 +20,14 @@ greet_user('camille')
 # Arguments and parameters
 # The variable 'username' above is a parameter: 
 # a piece of info the function needs to do its job.
-# The value 'camille' is an argument: a piece of info passed from a function call to a function.
-# The argument 'camille' was passed to the function greet_user() and the value was assigned to the parameter 'username'.
+# The value 'camille' is an argument: a piece of info passed from a function 
+# call to a function.
+# The argument 'camille' was passed to the function greet_user() 
+# and the value was assigned to the parameter 'username'.
 
 
-# Positional arguments. Python must match each argument in the function call with a parameter in the function definition.
+# Positional arguments. Python must match each argument in the function call 
+# with a parameter in the function definition.
 # Based on the order of the arguments provided
 def describe_pet(animal_type, pet_name):
     """Display information about a pet."""
@@ -34,3 +37,17 @@ def describe_pet(animal_type, pet_name):
 # Multiple function calls
 describe_pet('T-rex', 'Fred')
 describe_pet('rabbit', 'Marie')
+
+
+# Keyword Arguments
+# name-value pair passed to a function. We don't have to correctly order
+# the arguments in the function call and clarify the role of each value in the function call
+
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# Multiple function calls
+describe_pet(pet_name='Marie', animal_type='bear')
