84a63f9b5192b5154745373443af0d96b9dc2cd6
Camille Onoda <info@camilleonoda.com>
2023-09-17 11:53:59 +0900

    Initialize a Dog class

--- classes.py
+++ classes.py
@@ -11,8 +11,12 @@
 class Dog:
     # The __init__ method (function part of class is a method)
     # The __init__ method is run automatically whenever we create a new instance based on the Dog class
+    # The self parameter must always be included, in first position.
+    # It gives access to the attributes and methods in the class
     def __init__(self, name, age):
         # Initialize name and age attributes
+        # Variable accessible through instances are called 'attribute'
+        # Any variable created with self. is available to every method in the class
         self.name = name
         self.age = age
 
@@ -23,3 +27,14 @@ class Dog:
     def roll_over(self):
         # Simulate rolling over in response to commmand
         print(f"{self.name} rolled over!")
+
+
+
+# Making an instance of a class
+# A class is a set of isntructions for how to make an instance
+# Instance representing a specific dog:
+
+my_dog = Dog('Catou', 4)
+
+print(f"My dog's name is {my_dog.name}. ", end="")
+print(f"He is {my_dog.age} years old.")
