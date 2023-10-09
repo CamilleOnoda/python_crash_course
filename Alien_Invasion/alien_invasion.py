4dcec49a16d49ef197bb7f229b79f051f79df6c6
Camille Onoda <info@camilleonoda.com>
2023-10-09 14:19:46 +0900

    Set the background color to light gray

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -8,12 +8,13 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
-
         # Control the frame rate
         self.clock = pygame.time.Clock()
-
+        # Create a display window of 1200px wide and 800px high
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        # Set the background color
+        self.bg_color = (200, 200, 200)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -23,9 +24,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
             # Make the most recently drawn screen visible
             pygame.display.flip()
-            
+
             self.clock.tick(60)
 
 
